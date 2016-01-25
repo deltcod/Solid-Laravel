2fdf0f9ef937602a362fdc793dd3cdf348e7d86c
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-01-25 16:44:54 +0100

    Creating UserRepository

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adam
+ * Date: 25/01/16
+ * Time: 16:42
+ */
+
+namespace App\Repositories;
+
+
+use App\User;
+
+class UserRepository implements RepositoryInterface
+{
+
+    /**
+     * @param array $columns
+     * @return mixed
+     */
+    public function all()
+    {
+        return User::all();
+    }
+
+    /**
+     * @param int $perPage
+     * @param array $columns
+     * @return mixed
+     */
+    public function paginate($perPage = 15, $columns = array('*'))
+    {
+        // TODO: Implement paginate() method.
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    /**
+     * @param array $data
+     * @param $id
+     * @return mixed
+     */
+    public function update(array $data, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    /**
+     * @param $id
+     * @param array $columns
+     * @return mixed
+     */
+    public function find($id, $columns = array('*'))
+    {
+        // TODO: Implement find() method.
+    }
+
+    /**
+     * @param $field
+     * @param $value
+     * @param array $columns
+     * @return mixed
+     */
+    public function findBy($field, $value, $columns = array('*'))
+    {
+        // TODO: Implement findBy() method.
+    }
+}
